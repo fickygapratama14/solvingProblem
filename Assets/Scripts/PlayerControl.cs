2b84ce7eb948212058840feb915966d367c29142
fickygapratama14 <ficky.190170123@mhs.unimal.ac.id>
2021-11-02 10:23:44 +0700

    Problem-4

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+
+    public KeyCode upButton = KeyCode.W;
+
+    public KeyCode downButton = KeyCode.S;
+
+    public KeyCode kiriButton = KeyCode.A;
+
+    public KeyCode kananButton = KeyCode.D;
+
+    public float speed = 10.0f;   
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 velocity = rigidBody2D.velocity;
+
+        if (Input.GetKey(upButton))
+        {
+            velocity.y = speed;
+        }
+
+        else if (Input.GetKey(downButton))
+        {
+            velocity.y = -speed;
+        }
+
+        else if (Input.GetKey(kiriButton))
+        {
+            velocity.x = -speed;
+        }
+
+        else if (Input.GetKey(kananButton))
+        {
+            velocity.x = speed;
+        }
+
+        else
+        {
+            velocity.y = 0.0f;
+            velocity.x = 0.0f;
+        }
+        rigidBody2D.velocity = velocity;    
+    }
+}
