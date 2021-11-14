927de0d1cdcc8729452eee700637b58697f353e4
fickygapratama14 <ficky.190170123@mhs.unimal.ac.id>
2021-11-14 14:56:52 +0700

    Problem-10

---
+++ Assets/Scripts/BlockIsEnemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+    public class SegienamIsEnemy : MonoBehaviour
+    {     
+        public GameObject block;                
+        public float xMin, xMax, yMin, yMax;        
+
+        void Start ()
+        {
+            Spawn();
+        }
+        void Spawn ()
+        {
+            int spawnEnemy = Random.Range (4, 12);
+            for (int i =0; i < spawnEnemy; i++)
+            {
+                float randomX = Random.Range(xMin, xMax);
+                float randomY = Random.Range(yMin, yMax);
+                Instantiate(block, new Vector2(randomX, randomY), Quaternion.identity);
+            }
+            
+        }
+    }
---
+++ Assets/Scripts/RandomEnemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class RandomEnemy : MonoBehaviour
+    {     
+        public GameObject block;                
+        public float xMin, xMax, yMin, yMax;        
+
+        void Start ()
+        {
+            Spawn();
+            StartCoroutine(SpawnBlock());
+        }
+        void Spawn ()
+        {
+            int spawnEnemy = Random.Range (7, 14);
+            for (int i =0; i < spawnEnemy; i++)
+            {
+                float randomX = Random.Range(xMin, xMax);
+                float randomY = Random.Range(yMin, yMax);
+                Instantiate(block, new Vector2(randomX, randomY), Quaternion.identity);
+            }           
+        }
+        IEnumerator SpawnBlock()
+        {
+            if (transform.childCount < 12)
+            {
+                float randomX = Random.Range(xMin, xMax);
+                float randomY = Random.Range(yMin, yMax);
+                Instantiate(block, new Vector2(randomX, randomY), Quaternion.identity);
+            }
+            yield return new WaitForSeconds(3);
+            StartCoroutine(SpawnBlock());
+        }
+    }
